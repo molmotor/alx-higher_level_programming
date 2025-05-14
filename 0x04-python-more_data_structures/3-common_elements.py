ec83328435520eb10c5a40efe695e66fd7cee4a0
molmotor <haitamessaid03@gmail.com>
2025-05-14 21:52:01 +0100

    hj

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     for item in set_1:
-        if item not in set_2:
-            pass
-        else:
-            c_set.append(item)
+        if item in set_2:
+            c_list.append(item)
     
     return c_set
