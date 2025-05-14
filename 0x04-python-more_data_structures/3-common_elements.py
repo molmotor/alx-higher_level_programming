57b19318dfa70844c3fca2759d65946553fc3910
molmotor <haitamessaid03@gmail.com>
2025-05-14 21:47:22 +0100

    hj

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    for item in set_1:
+        if item not in set_2:
+            pass
+        else:
+            c_set.append(item)
+    
+    return c_set
