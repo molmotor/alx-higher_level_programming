2151c840d16544deb42b704c8eb5889ac3d25789
molmotor <haitamessaid03@gmail.com>
2025-05-14 21:21:29 +0100

    hj

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    temp_list = []
+    for element in my_list:
+        for a in temp_list = []:
+            if element != a:
+                temp_list.append(a)
+                new_list = new_list + element
+            else:
+                pass
