d3fbc754c1e10f90fd07c72453287e51bd76692b
molmotor <haitamessaid03@gmail.com>
2025-05-14 21:37:30 +0100

    hj

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     new_list = []
     total = 0
 
-    for element in my_list:
+    for element in new_list:
         if element not in my_list:
             new_list.append(element)
             total += element
