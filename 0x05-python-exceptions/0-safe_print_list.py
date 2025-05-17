d7572047ffdba4ccbc37b9bcefc413bd3e0469ae
molmotor <haitamessaid03@gmail.com>
2025-05-17 21:18:22 +0100

    h

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-def safe_^rint_list(my_list=[], x=0):
-    for element in my_list:
-        count = 0
-
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
         for i in range(x):
-            print("{}".format(list[x]))
+            print("{}".format(my_list[i]), end="")
             count += 1
-        except IndexError:
-            pass
-
-        print()
-        return count
+    except IndexError:
+        pass
+    print()
+    return count
