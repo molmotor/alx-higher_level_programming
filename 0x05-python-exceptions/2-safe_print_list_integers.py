2bb3ec5881cd1bfa8e85f06c1439b4f053f5e70d
molmotor <haitamessaid03@gmail.com>
2025-05-19 09:32:18 +0100

    hj

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for element in my_list:
         for i in range(x):
             try:
                 if my_list[i] == int():
