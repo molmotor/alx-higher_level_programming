829aa2c74bf78e7617b6915a6e7ef4fc88f67588
molmotor <haitamessaid03@gmail.com>
2025-06-30 00:10:00 +0100

    hj

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
         except(TypeError, ZeroDivisionError):
            c = None
         finally:
-           print("{:.2f}".format(c) if c is not none else "None")
+           print("{:.2f}".format(c) if c is not None else "None")
         return c
